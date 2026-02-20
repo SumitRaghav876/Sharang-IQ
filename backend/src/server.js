8164373e3a0199d3a15f76c2d2b145f021e9705e
SumitRaghav876 <raghavsumitkr@gmail.com>
2026-02-20 10:36:10 +0530

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import path from "path";
+
+import { ENV } from "./lib/env.js";
+
+const app=express();
+const __dirname = path.resolve();
+
+app.get("/home",(req,res)=>{
+    res.send("working properly fine?")
+})
+// deployment ready
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(ENV.PORT,()=>{
+    console.log("app is listenning on port",ENV.PORT);
+});
