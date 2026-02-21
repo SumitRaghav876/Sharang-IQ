8acc3ce572bbba09750cd8aaa63d093fc652b94a
SumitRaghav876 <raghavsumitkr@gmail.com>
2026-02-21 16:55:43 +0530

    inngest  issue resolve

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,11 +9,11 @@ import { functions, inngest } from "./lib/inngest.js";
 const app=express();
 const __dirname = path.resolve();
 
-app.use("/api/inngest",serve({client:inngest, functions}));
-
 //middleware
 app.use(express.json());
 app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
+
+app.use("/api/inngest",serve({client:inngest, functions}));
 
 app.get("/home",(req,res)=>{
     res.send("working properly fine?")
