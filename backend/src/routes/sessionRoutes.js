608079e6711aeff38f3c1ad208d4f44476aa528f
SumitRaghav876 <raghavsumitkr@gmail.com>
2026-02-26 19:09:59 +0530

    Session route and controllers

---
+++ backend/src/routes/sessionRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+import { createSession, endSession, getActiveSessions, getRecentSessions, getSessionById, joinSession } from "../controllers/sessionController.js";
+
+const router=express.Router();
+
+router.post("/",protectRoute,createSession);
+router.get("/active",protectRoute,getActiveSessions);
+router.get("/recents",protectRoute,getRecentSessions);
+
+router.get("/:id",protectRoute,getSessionById);
+router.post("/:id/join",protectRoute,joinSession);
+router.post("/:id/end",protectRoute,endSession);
+
+export default router;
